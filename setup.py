5b9ba450051aa5dd7e5844901002022fd507d3b2
Alistair Johnson <alistairewj@gmail.com>
2019-04-12 14:49:25 -0400

    pip installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='bert_deid',
+    version='0.1',
+    description='Remove identifiers from data using BERT',
+    url='https://github.com/alistairewj/bert-deid',
+    author='Alistair Johnson',
+    author_email='aewj@mit.edu',
+    license='Apache 2.0',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+)
