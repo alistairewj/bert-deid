112d7b5a7e419b3b9d63492d85fb2a2c12fa9a93
Tom Pollard <tpollard@mit.edu>
2019-09-26 13:36:36 -0400

    tests passing

--- bert_deid/model.py
+++ bert_deid/model.py
@@ -109,12 +109,17 @@ class BertForNER(BertForTokenClassification):
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None,
                 labels=None, position_ids=None, head_mask=None):
-        outputs = self.bert(input_ids, position_ids=position_ids,
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
                             token_type_ids=token_type_ids,
-                            attention_mask=attention_mask, head_mask=head_mask)
+                            position_ids=position_ids,
+                            head_mask=head_mask)
+
         sequence_output = outputs[0]
 
         sequence_output = self.dropout(sequence_output)
+
         logits = self.classifier(sequence_output)
 
         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
@@ -130,30 +135,9 @@ class BertForNER(BertForTokenClassification):
                 loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
             outputs = (loss,) + outputs
 
-        return outputs  # (loss), scores, (hidden_states), (attentions)
+        # outputs  # (loss), scores, (hidden_states), (attentions)
 
-
-
-        sequence_output, _ = self.bert(
-            input_ids, token_type_ids, attention_mask,
-            output_all_encoded_layers=False)
-        sequence_output = self.dropout(sequence_output)
-        logits = self.classifier(sequence_output)
-
-        if labels is not None:
-            loss_fct = CrossEntropyLoss(ignore_index=-1)
-            # Only keep active parts of the loss
-            if attention_mask is not None:
-                active_loss = attention_mask.view(-1) == 1
-                active_logits = logits.view(-1, self.num_labels)[active_loss]
-                active_labels = labels.view(-1)[active_loss]
-                loss = loss_fct(active_logits, active_labels)
-            else:
-                loss = loss_fct(
-                    logits.view(-1, self.num_labels), labels.view(-1))
-            return loss
-        else:
-            return logits
+        return logits
 
 
 class BertForDEID(BertForNER):
@@ -306,8 +290,7 @@ class BertForDEID(BertForNER):
             segment_ids = segment_ids.to(device)
 
         with torch.no_grad():
-            outputs = self.forward(input_ids, segment_ids, input_mask)
-            logits = outputs[1]
+            logits = self.forward(input_ids, segment_ids, input_mask)
 
         if next(self.parameters()).is_cuda:
             logits = logits.cpu().numpy()
--- tests/models/i2b2_2014/README.md
+++ tests/models/i2b2_2014/README.md
@@ -4,4 +4,4 @@ To run the tests, upload the pretrained model `pytorch_model.bin` to this folder
 
 ## The model should match the following MD5 hash
 
-MD5 (pytorch_model.bin) = 503e6eee66a4799da9d623142e7bc8ae
+tests/models/i2b2_2014/pytorch_model.bin = 503e6eee66a4799da9d623142e7bc8ae
