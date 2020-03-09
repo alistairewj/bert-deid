d424791086695be1b0932aeebf1c8bdb05903f85
Alistair Johnson <aewj@mit.edu>
2020-03-09 15:39:37 -0400

    move utility functions to utils module

--- scripts/calculate_performance.py
+++ scripts/calculate_performance.py
@@ -124,234 +124,6 @@ def argparser():
     return args
 
 
-def get_characterwise_labels(label_set, text):
-    """
-    Given a label collections, outputs an integer vector with the label_id.
-    
-    Integer vectors are the same length as the text.
-    """
-    # integer vector indicating truth
-    label_ids = -1 * np.ones(len(text), dtype=int)
-    for label in label_set.labels:
-        label_ids[label.start:label.start +
-                  label.length] = label_set.label_to_id[label.entity_type]
-
-    return label_ids
-
-
-def split_iterator(pattern, text):
-    """
-    Iterator that splits text using a regex pattern.
-    
-    Returns
-    -------
-    token, start, stop
-        Tuple containing the token, the start index of the token
-        in the original string, and the end index of the
-    """
-
-    tokens = pattern.split(text)
-
-    offset = 0
-    for token in tokens:
-        offset = text.find(token, offset)
-        yield token, offset, offset + len(token)
-        offset += len(token)
-
-
-def split_with_offsets(pattern, text):
-    """
-    Function that wraps the pattern span iterator.
-    """
-    tokens_with_spans = list()
-    for token, start, end in split_iterator(pattern, text):
-        tokens_with_spans.append([start, end, token])
-
-    return tokens_with_spans
-
-
-def mode(values, ignore_value=None):
-    """Get the most frequent value, ignoring a specified value if desired."""
-    if len(values) == 0:
-        raise ValueError('Cannot calculate mode of length 0 array.')
-
-    p_unique, p_counts = np.unique(values, return_counts=True)
-    # remove our ignore index
-    if ignore_value is not None:
-        idx = np.where(p_unique == ignore_value)[0]
-        if len(idx) > 0:
-            # we know p_unique is unique, so delete the only element of idx
-            p_unique = np.delete(p_unique, idx[0])
-            p_counts = np.delete(p_counts, idx[0])
-
-    return p_unique[np.argmax(p_counts)]
-
-
-def expand_id_to_token(token_pred, ignore_value=None):
-    # get most frequent label_id for this token
-    p_unique, p_counts = np.unique(token_pred, return_counts=True)
-
-    if len(p_unique) <= 1:
-        return token_pred
-
-    # remove our ignore index
-    if ignore_value is not None:
-        idx = np.where(p_unique == ignore_value)[0]
-        if len(idx) > 0:
-            # we know p_unique is unique, so get the only element
-            p_unique = np.delete(p_unique, idx[0])
-            p_counts = np.delete(p_counts, idx[0])
-
-    if len(p_unique) == 1:
-        idx = 0
-    else:
-        # TODO: warn user if we broke a tie by taking lowest ID
-        idx = np.argmax(p_counts)
-
-    # re-create the array with only the most frequent label
-    token_pred = np.ones(len(token_pred), dtype=int) * p_unique[idx]
-    return token_pred
-
-
-def generate_token_arrays(
-    text,
-    text_tar,
-    text_pred,
-    tokenizer=None,
-    expand_predictions=True,
-    split_true_entities=True,
-    ignore_value=None
-):
-    """
-    Evaluate performance of prediction labels compared to ground truth.
-
-
-    Args
-        text_tar - N length numpy array with integers for ground truth labels
-        text_pred - N length numpy array with integers for predicted labels
-        tokenizer - Determines the granularity level of the evaluation.
-            None or '' - character-wise evaluation
-            r'\w' - word-wise evaluation
-        expand_predictions - If a prediction is partially made for a
-            token, expand it to cover the entire token. If not performed,
-            then partially labeled tokens are treated as missed detections.
-        split_true_entities - The ground truth label for a single token
-            may correspond to two distinct classes (e.g. if word splitting,
-            John/2010 would be one token but have two ground truth labels).
-            Enabling this argument splits these tokens.
-        ignore_value - Ignore a label_id in the evaluation. Useful for ignoring
-            the 'other' category.
-    """
-    # split text for token evaluation
-    if isinstance(tokenizer, stanfordnlp.pipeline.core.Pipeline):
-        doc = tokenizer(text)
-        # extract tokens from the parsed text
-        tokens_base = [
-            token.text for sentence in doc.sentences
-            for token in sentence.tokens
-        ]
-    elif isinstance(tokenizer, spacy.tokenizer.Tokenizer):
-        doc = tokenizer(text)
-        # extract tokens from the parsed text
-        tokens_base = [token.text for token in doc]
-    else:
-        if tokenizer is None:
-            tokenizer = ''
-        # treat string as a regex
-        tokens_base = re.findall(tokenizer, text)
-
-    tokens = []
-    tokens_pred = []
-    tokens_true = []
-    tokens_start, tokens_length = [], []
-
-    n_tokens = 0
-
-    start = 0
-    for token in tokens_base:
-        # sometimes we have empty tokens on their own
-        if len(token) == 0:
-            continue
-        start = text.find(token, start)
-        token_true = text_tar[start:start + len(token)]
-        token_pred = text_pred[start:start + len(token)]
-
-        if all(token_true == -1) & all(token_pred == -1):
-            # skip tokens which are not labeled
-            start += len(token)
-            n_tokens += 1
-            continue
-
-        if split_true_entities:
-            # split the single token into subtokens, based on the true entity
-            idxDiff = np.diff(token_true, prepend=0)
-            if any(idxDiff > 0):
-                # split
-                idxDiff = np.diff(token_true, prepend=0)
-                subtok_start = 0
-                subtoken_true, subtoken_pred = [], []
-                for subtok_end in np.where(idxDiff > 0)[0]:
-                    subtoken_true.append(token_true[subtok_start:subtok_end])
-                    subtoken_pred.append(token_pred[subtok_start:subtok_end])
-                    subtok_start = subtok_end
-                if subtok_end < len(token_true):
-                    # add final token
-                    subtoken_true.append(token_true[subtok_start:])
-                    subtoken_pred.append(token_pred[subtok_start:])
-            else:
-                # in this case, there is only 1 label_id for the entire token
-                # so we can just wrap in a list for the iterator later
-                subtoken_true = [token_true]
-                subtoken_pred = [token_pred]
-        else:
-            # do not split a token if there is more than 1 ground truth
-            # consequently, tokens with multiple labels will be treated
-            # as equal to the most frequent label
-            subtoken_true = [token_true]
-            subtoken_pred = [token_pred]
-
-        # now iterate through our sub-tokens
-        # often this is a length 1 iterator
-        for token_true, token_pred in zip(subtoken_true, subtoken_pred):
-            if len(token_true) == 0:
-                continue
-
-            if expand_predictions:
-                # expand the most frequent ID to cover the entire token
-                token_pred = expand_id_to_token(token_pred, ignore_value=-1)
-                token_true = expand_id_to_token(token_true, ignore_value=-1)
-
-            # get the length of the token for later
-            token_len = len(token_true)
-
-            # aggregate IDs for this token into the most frequent value
-            if len(token_true) == 0:
-                token_true = -1
-            else:
-                token_true = mode(token_true, ignore_value)
-            if len(token_pred) == 0:
-                token_pred = -1
-            else:
-                token_pred = mode(token_pred, ignore_value)
-
-            # append the prediction for this token
-            tokens_true.append(token_true)
-            tokens_pred.append(token_pred)
-            tokens.append(text[start:start + token_len])
-            tokens_start.append(start)
-            tokens_length.append(token_len)
-
-            start += token_len
-            # keep track of total tokens assessed
-            n_tokens += 1
-
-    # now we have a list of tokens with preds
-    tokens_true = np.asarray(tokens_true, dtype=int)
-    tokens_pred = np.asarray(tokens_pred, dtype=int)
-
-    return tokens_true, tokens_pred, tokens, tokens_start, tokens_length, n_tokens
-
-
 def main():
     args = argparser()
 
@@ -463,12 +235,12 @@ def main():
             fn_pred = pred_folder / f'{fn}{pred_ext}'
             pred.from_csv(fn_pred)
 
-            text_tar = get_characterwise_labels(gs, text)
-            text_pred = get_characterwise_labels(pred, text)
+            text_tar = utils.get_characterwise_labels(gs, text)
+            text_pred = utils.get_characterwise_labels(pred, text)
 
             # now evaluate the character-wise labels using some aggregation
             # an empty tokenizer results in character-wise evaluation
-            tokens_true, tokens_pred, tokens, tokens_start, tokens_length, n_tokens = generate_token_arrays(
+            tokens_true, tokens_pred, tokens, tokens_start, tokens_length = utils.generate_token_arrays(
                 text,
                 text_tar,
                 text_pred,
@@ -492,7 +264,6 @@ def main():
 
             performance = {}
             performance['model'] = pred_folder.stem
-            performance['n_tokens'] = n_tokens
             performance['n_token_phi'] = len(tokens_true)
             performance['n_true_positive'] = sum(
                 (tokens_true >= 0) & (tokens_true == tokens_pred)
