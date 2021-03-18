a9591ef2230c7c9b31377eff204a8d15a203613a
Alistair Johnson <alistairewj@gmail.com>
2021-03-18 13:58:37 -0400

    add requirements to setup

--- setup.py
+++ setup.py
@@ -14,7 +14,13 @@ setuptools.setup(
     author_email='aewj@mit.edu',
     license='Apache 2.0',
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+        'nltk>=3.4.5', 'mpmath>=1.1.0', 'numpy>=1.19.2', 'pandas>=1.1.3',
+        'pytest>=4.2.0', 'pytorch>=1.6.0', 'scikit-learn>=0.23.2',
+        'spacy>=2.3.2', 'sympy>=1.6.2', 'tqdm>=4.32.1', 'regex>=2020.10.23',
+        'transformers>=3.4.0', 'tokenizers>=0.9.2', 'stanfordnlp>=0.2.0',
+        'google-cloud-storage>=1.32.0'
+    ],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': ['bert_deid = bert_deid.__main__:main'],
