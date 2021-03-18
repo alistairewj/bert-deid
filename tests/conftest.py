e9d90f4d11282f1a139c29ed4e7dfbac4dce2392
Alistair Johnson <alistairewj@gmail.com>
2021-03-18 13:10:22 -0400

    refactor tests to work with v0.2.3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,72 +1,43 @@
-# Fixtures used by various tests
+"""Fixtures used by various tests - primarily to load the model and example text."""
 # Fixtures are a good way to
 #  - load and share data across tests
 #  - inject dependencies into tests
 # We use environment variables in fixtures.
 # If not present, we use default values.
-import json
 import os
-
 import pytest
-from bert_deid.model import Transformer
+from bert_deid.model.transformer import Transformer
 
 
 @pytest.fixture(scope="session")
-def config_radiology_reports():
-    """
-    Load configuration file for the test radiology dataset.
-    """
-    # get path of conftest.py
+def ds_text():
+    """Example text of a discharge summary."""
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, 'example_note.txt'), 'r') as fp:
+        text = ''.join(fp.readlines())
 
-    # load the config json into a dictionary
-    config_fn = os.path.join(
-        dir_path, 'fake-data', 'config-radiology-reports.json'
-    )
-
-    # if config provided, update dict with values from config
-    with open(config_fn, 'r') as fp:
-        config = json.load(fp)
-
-    return config
+    return text
 
 
 @pytest.fixture(scope="session")
-def radiology_reports(config_radiology_reports):
-    """
-    Load the test radiology reports.
-    """
-    text_path = config_radiology_reports['text_path']
+def ds_text_deid():
+    """Deidentified text of a discharge summary."""
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, 'example_note_deid.txt'), 'r') as fp:
+        text = ''.join(fp.readlines())
 
-    reports_list = os.listdir(text_path)
-    reports_list.sort()
-
-    assert len(reports_list) == 7
-
-    # load reports
-    reports = {}
-    for f in reports_list:
-        with open(os.path.join(text_path, f), 'r') as fp:
-            reports[f] = ''.join(fp.readlines())
-
-    assert len(reports) == 7
-
-    return reports
+    return text
 
 
 @pytest.fixture(scope="session")
 def bert_i2b2_2014_model():
     """
-    Load the model.
+    The BERT-deid model trained on the i2b2-2014 deid challenge corpus.
     """
-    model_type = 'bert'  # -base-uncased'
-
-    model_path = os.getenv('MODEL_PATH')
+    model_path = os.getenv('MODEL_DIR')
     if model_path is None:
         # use default
         model_path = '/data/models/bert-i2b2-2014'
 
-    transformer = Transformer(
-        model_type, model_path, max_seq_length=128, device='cpu'
-    )
+    transformer = Transformer(model_path, device='cpu')
     return transformer
--- tests/test_bert_apply.py
+++ tests/test_bert_apply.py
@@ -1,42 +1,32 @@
-# load in a trained model
+"""Test applying bert to a set of text."""
 
 
-def test_model_predict(bert_i2b2_2014_model):
-    text = 'Dr. Michelle Obama says I have had a pneumothorax since 2019-01-01.'
+def test_model_predict(bert_i2b2_2014_model, ds_text):
     truth = [
-        ['NAME', 4, 8], ['NAME', 13, 5], ['DATE', 56, 4], ['DATE', 60, 1],
-        ['DATE', 61, 2], ['DATE', 63, 1], ['DATE', 64, 2]
+        ['NAME', 7, 8], ['NAME', 11, 4], ['NAME', 14, 1], ['NAME', 15, 1],
+        ['NAME', 16, 5], ['ID', 50, 8], ['ID', 53, 5], ['ID', 55, 3],
+        ['ID', 57, 1], ['DATE', 77, 2], ['DATE', 79, 1], ['DATE', 80, 2],
+        ['DATE', 82, 1], ['DATE', 83, 2], ['DATE', 117, 2], ['DATE', 119, 1],
+        ['DATE', 120, 2], ['DATE', 122, 1], ['DATE', 123, 2], ['DATE', 143, 2],
+        ['DATE', 145, 1], ['DATE', 146, 2], ['DATE', 148, 1], ['DATE', 149, 4],
+        ['NAME', 463, 8], ['NAME', 467, 4], ['NAME', 470, 1], ['AGE', 486, 3],
+        ['NAME', 511, 8], ['DATE', 533, 2], ['DATE', 535, 1], ['DATE', 536, 2],
+        ['DATE', 538, 1], ['DATE', 539, 2], ['DATE', 909, 2], ['DATE', 911, 1],
+        ['DATE', 912, 2], ['DATE', 914, 1], ['DATE', 915, 2], ['DATE', 1422, 2],
+        ['DATE', 1424, 1], ['DATE', 1425, 2], ['DATE', 1428, 2]
     ]
 
-    preds, lengths, offsets = bert_i2b2_2014_model.predict(text)
+    labels = bert_i2b2_2014_model.predict(ds_text)
 
-    # get the free-text label
-    labels = [
-        bert_i2b2_2014_model.label_set.id_to_label[idxMax]
-        for idxMax in preds.argmax(axis=1)
-    ]
-    outputs = [
-        [labels[i], offsets[i], lengths[i]]
-        for i in range(len(preds)) if labels[i] != 'O'
-    ]
-
-    for l in range(len(outputs)):
-        pred = outputs[l]
-        target = truth[l]
+    for l, target in enumerate(truth):
+        # skip probability in first element for preds
+        pred = labels[l][1:]
         assert pred[0] == target[0], 'label incorrect'
         assert pred[1] == target[1], 'offset incorrect'
         assert pred[2] == target[2], 'length incorrect'
 
 
-def test_model_apply(bert_i2b2_2014_model):
-    text = 'Hello my name is Dr. Somayah, I have worked here since 2019.'
-    text_deid = 'Hello my name is Dr. ___, I have worked here since ___.'
+def test_model_apply(bert_i2b2_2014_model, ds_text, ds_text_deid):
+    text_deid = bert_i2b2_2014_model.apply(ds_text)
 
-    assert text_deid == bert_i2b2_2014_model.apply(text, repl='___')
-
-
-def test_model_apply_multiple_entities(bert_i2b2_2014_model):
-    text = 'Patient is a 64 yo male named Richard the Lionheart.'
-    text_deid = 'Patient is a ___ yo male named ___.'
-
-    assert text_deid == bert_i2b2_2014_model.apply(text, repl='___')
+    assert text_deid == ds_text_deid
